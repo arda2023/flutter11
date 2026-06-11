abf34479aee8976c1364a12b28b8a632245638e0
Arda <ardasa716@gmail.com>
2026-06-11 13:25:39 +0200

    ih

---
+++ lib/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter11/plusminus.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(18),
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(28),
+                  color: Colors.blue,
+                ),
+                height: 350,
+              ),
+              Plusminus(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/plusminus.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
+
+class Plusminus extends StatefulWidget {
+  const Plusminus({super.key});
+
+  @override
+  _PlusminusState createState() => _PlusminusState();
+}
+
+class _PlusminusState extends State<Plusminus> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        children: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.set_meal)),
+          ShadIconButton.outline(
+            icon: const Icon(LucideIcons.rocket),
+            onPressed: () => print('Outline'),
+          ),
+        ],
+      ),
+    );
+  }
+}
