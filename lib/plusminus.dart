79b4490c5f26de386164a04ae655b4c9001542e9
Arda <ardasa716@gmail.com>
2026-06-15 21:16:37 +0200

    ijij

--- lib/plusminus.dart
+++ lib/plusminus.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
+const List<int> _optionen = <int>[250, 500, 1000];
+const double _kItemExtent = 32.0;
+
 class Plusminus extends StatefulWidget {
   const Plusminus({
     super.key,
@@ -19,6 +22,7 @@ class Plusminus extends StatefulWidget {
 }
 
 class _PlusminusState extends State<Plusminus> {
+  final int _selectedOption = 0;
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -40,38 +44,44 @@ class _PlusminusState extends State<Plusminus> {
         const SizedBox(width: 12),
 
         // 2. Mittlerer Anzeige-Container
-        Expanded(
-          child: Container(
-            height: 48, // Gleiche Höhe wie die Buttons
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-              borderRadius: BorderRadius.circular(
-                12,
-              ), // Leicht abgerundet wie im Bild
-              // Eine sehr dezente graue Umrandung, passend zu Shadcn UI Outlines
-              border: Border.all(width: 1, color: Colors.grey.shade200),
-            ),
-            child: Center(
-              child: ExpansionTile(
-                title: Text("${widget.currentStep}"),
-                children: [
-                  ListTile(
-                    title: Text("500ml"),
-                    onTap: () {
-                      widget.onStepSelected(500);
-                    },
-                  ),
-                  ListTile(
-                    title: Text("1L"),
-                    onTap: () {
-                      widget.onStepSelected(1000);
-                    },
-                  ),
-                ],
+        /* Expanded(
+          child: GestureDetector(
+            onTap: () {
+              showCupertinoModalPopup(
+                context: context,
+                builder: (BuildContext context) {
+                  return Container(
+                    height: 250,
+                    color: Colors.white,
+                    child: CupertinoPicker(
+                      itemExtent: _kItemExtent,
+                      onSelectedItemChanged: (int selectedItem) {
+                        setState(() {
+                          _selectedOption = selectedItem;
+                        });
+                      },
+
+                      children: _optionen.map((int wert) {
+                        return Text("$wert ml");
+                      }).toList(),
+                    ),
+                  );
+                },
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(
+                  12,
+                ), // Leicht abgerundet wie im Bild
+                // Eine sehr dezente graue Umrandung, passend zu Shadcn UI Outlines
+                border: Border.all(width: 1, color: Colors.grey.shade200),
               ),
+              child: Center(child: Text("${widget.currentStep} ml")),
             ),
           ),
-        ),
+        ), */
 
         // Abstand zwischen Textbox und Plus
         const SizedBox(width: 12),
