c00da82244168db75e0048dccf943a96cf84779b
Arda <ardasa716@gmail.com>
2026-06-17 16:58:47 +0200

    ii

---
+++ lib/widgets/fab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Fab extends StatefulWidget {
+  const Fab({super.key});
+
+  @override
+  _FabState createState() => _FabState();
+}
+
+class _FabState extends State<Fab> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: null);
+  }
+}
