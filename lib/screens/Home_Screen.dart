13ebb1d988cff35c6a426a1d8ff6c52ce80023f3
Arda <ardasa716@gmail.com>
2026-06-21 15:11:20 +0200

    ioo2

--- lib/screens/Home_Screen.dart
+++ lib/screens/Home_Screen.dart
@@ -1,8 +1,8 @@
+import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter11/screens/Home_Content.dart';
 import 'package:flutter11/screens/Stats.dart';
 import 'package:flutter11/screens/settings.dart';
-import 'package:flutter11/widgets/navbar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,25 +12,50 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final List<Widget> pages = [
-    const HomeContent(),
-    const StatsPage(),
-    const SettingsPage(),
-  ];
+  final NotchBottomBarController controller = NotchBottomBarController();
+
+  final List<Widget> pages = const [HomeContent(), StatsPage(), SettingsPage()];
+
   int currentPageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Navbar(
-        currentIndex: currentPageIndex,
-        onDestinationSelected: (index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-      ),
-
       body: pages[currentPageIndex],
+
+      bottomNavigationBar: SizedBox(
+        height: 100,
+        child: AnimatedNotchBottomBar(
+          notchBottomBarController: controller,
+
+          kIconSize: 24,
+          kBottomRadius: 28,
+
+          bottomBarItems: const [
+            BottomBarItem(
+              inActiveItem: Icon(Icons.home_outlined),
+              activeItem: Icon(Icons.home),
+              itemLabel: 'Home',
+            ),
+            BottomBarItem(
+              inActiveItem: Icon(Icons.bar_chart_outlined),
+              activeItem: Icon(Icons.bar_chart),
+              itemLabel: 'Stats',
+            ),
+            BottomBarItem(
+              inActiveItem: Icon(Icons.settings_outlined),
+              activeItem: Icon(Icons.settings),
+              itemLabel: 'Settings',
+            ),
+          ],
+
+          onTap: (index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+        ),
+      ),
     );
   }
 }
--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter11/models/navbarpainter.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
-class Navbar extends StatefulWidget {
+class Navbar extends StatelessWidget {
   final ValueChanged<int> onDestinationSelected;
   final int currentIndex;
 
@@ -13,168 +11,27 @@ class Navbar extends StatefulWidget {
   });
 
   @override
-  State<Navbar> createState() => _NavbarState();
-}
-
-class _NavbarState extends State<Navbar> with SingleTickerProviderStateMixin {
-  late AnimationController controller;
-  late Animation<double> animation;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 350),
-    );
-    animation = AlwaysStoppedAnimation(widget.currentIndex.toDouble());
-  }
-
-  @override
-  void didUpdateWidget(Navbar oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.currentIndex != widget.currentIndex) {
-      animation =
-          Tween<double>(
-            begin: oldWidget.currentIndex.toDouble(),
-            end: widget.currentIndex.toDouble(),
-          ).animate(
-            CurvedAnimation(parent: controller, curve: Curves.easeInOutCubic),
-          );
-
-      controller.forward(from: 0.0);
-    }
-  }
-
-  // Wir fügen hier auch direkt das Saubermachen hinzu, um Abstürze zu verhindern:
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    print("${widget.currentIndex}");
-    // Berechne die Breite eines Segments basierend auf dem verfügbaren Platz:
-    // Da wir links und rechts 24px Padding haben, nehmen wir die Bildschirmbreite minus 48
-    final double gesamtBreite = MediaQuery.of(context).size.width - 48;
-    final double segmentBreite = gesamtBreite / 3;
-    final double aktivesZielX =
-        (segmentBreite * widget.currentIndex) + (segmentBreite / 2);
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(24, 0, 24, 24),
-      child: Container(
-        height: 75,
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withValues(alpha: 0.1),
-              blurRadius: 12,
-              offset: const Offset(0, 5),
-            ),
-          ],
-        ),
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: AnimatedBuilder(
-                animation: controller,
-                builder: (context, child) {
-                  return CustomPaint(
-                    painter: NavbarPainter(
-                      selectedIndex:
-                          animation.value, // Der fließende Komma-Wert
-                      totalItems: 3,
-                    ),
-                  );
-                },
-              ),
-            ),
-            Positioned.fill(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  buildNavItem(Icons.home_outlined, Icons.home, "Home", 0),
-                  buildNavItem(
-                    Icons.numbers_outlined,
-                    Icons.numbers,
-                    "Stats",
-                    1,
-                  ),
-                  buildNavItem(
-                    Icons.settings_outlined,
-                    Icons.settings,
-                    "Settings",
-                    2,
-                  ),
-                ],
-              ),
-            ),
-            AnimatedPositioned(
-              duration: const Duration(milliseconds: 350),
-              curve: Curves.easeInOutCubic,
-              left: aktivesZielX - 12,
-              top: -10,
-              child: SvgPicture.asset(
-                "assets/tropfen.svg",
-                width: 24,
-                height: 24,
-                colorFilter: ColorFilter.mode(
-                  Colors.purple.shade900,
-                  BlendMode.srcIn,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return Scaffold(body: null);
   }
-
-  Widget buildNavItem(
-    IconData icon,
-    IconData activeIcon,
-    String label,
-    int index,
-  ) {
-    final isSelected = widget.currentIndex == index;
-
-    return InkWell(
-      onTap: () {
-        print("geklickt: $index");
-        widget.onDestinationSelected(index);
-      },
-      child: AnimatedContainer(
-        duration: Duration(milliseconds: 300),
-        curve: Curves.easeOutBack,
-
-        width: 80,
-        child: Padding(
-          padding: EdgeInsets.only(top: isSelected ? 15 : 0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                isSelected ? activeIcon : icon,
-                color: Colors.purple.shade900,
-                size: 26,
-              ),
-              const SizedBox(height: 4),
-              Text(
-                label,
-                style: TextStyle(
-                  color: Colors.purple.shade900,
-                  fontSize: 12,
-                  fontWeight: isSelected ? FontWeight.w600 : FontWeight.normal,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  // ...
 }
+/*BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onDestinationSelected,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          activeIcon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.numbers_outlined),
+          activeIcon: Icon(Icons.numbers),
+          label: 'Stats',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings_outlined),
+          activeIcon: Icon(Icons.settings),
+          label: 'Settings',
+        ),
+      ],*/
