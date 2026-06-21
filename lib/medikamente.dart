157e25f8772c23989b450efff778ca19ad1f6817
Arda <ardasa716@gmail.com>
2026-06-21 18:33:24 +0200

    feat: adhs medikamenten datenbank und meds_search_box hinzugefuegt

---
+++ lib/medikamente.dart
@@ -0,0 +1,91 @@
+// lib/data/medikamente_data.dart
+
+const List<Map<String, dynamic>> adhsDatenbank = [
+  // --- Amphetamin-basierte Stimulanzien ---
+  {
+    "name": "Elvanse",
+    "dosierungen": ["20 mg", "30 mg", "40 mg", "50 mg", "60 mg", "70 mg"],
+  },
+  {
+    "name": "Elvanse Adult",
+    "dosierungen": ["30 mg", "50 mg", "70 mg"],
+  },
+  {
+    "name": "Attentin",
+    "dosierungen": ["5 mg", "10 mg", "20 mg"],
+  },
+  {
+    "name": "Kinecteen",
+    "dosierungen": ["18 mg", "27 mg", "36 mg", "54 mg"],
+  },
+
+  // --- Methylphenidat-basierte Stimulanzien (Retardiert) ---
+  {
+    "name": "Medikinet retard",
+    "dosierungen": [
+      "5 mg",
+      "10 mg",
+      "20 mg",
+      "30 mg",
+      "40 mg",
+      "50 mg",
+      "60 mg",
+    ],
+  },
+  {
+    "name": "Medikinet Adult",
+    "dosierungen": [
+      "5 mg",
+      "10 mg",
+      "20 mg",
+      "30 mg",
+      "40 mg",
+      "50 mg",
+      "60 mg",
+    ],
+  },
+  {
+    "name": "Ritalin LA",
+    "dosierungen": ["10 mg", "20 mg", "30 mg", "40 mg", "60 mg"],
+  },
+  {
+    "name": "Ritalin Adult",
+    "dosierungen": ["10 mg", "20 mg", "30 mg", "40 mg", "50 mg", "60 mg"],
+  },
+  {
+    "name": "Concerta",
+    "dosierungen": ["18 mg", "27 mg", "36 mg", "54 mg"],
+  },
+  {
+    "name": "Equasym Retard",
+    "dosierungen": ["10 mg", "20 mg", "30 mg"],
+  },
+
+  // --- Unretardiertes Methylphenidat (Schnell wirksam) ---
+  {
+    "name": "Medikinet (unretardiert)",
+    "dosierungen": ["5 mg", "10 mg", "20 mg"],
+  },
+  {
+    "name": "Ritalin (unretardiert)",
+    "dosierungen": ["10 mg"],
+  },
+
+  // --- Nicht-Stimulanzien ---
+  {
+    "name": "Atomoxetin (z.B. Strattera)",
+    "dosierungen": [
+      "10 mg",
+      "18 mg",
+      "25 mg",
+      "40 mg",
+      "60 mg",
+      "80 mg",
+      "100 mg",
+    ],
+  },
+  {
+    "name": "Intuniv (Guanfacin)",
+    "dosierungen": ["1 mg", "2 mg", "3 mg", "4 mg"],
+  },
+];
